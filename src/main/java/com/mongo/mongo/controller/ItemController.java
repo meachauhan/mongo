ad15ae9256e3a9881f82e352e724ac5f809903c3
Akash Chauhan <meachauhan@gmail.com>
2024-08-28 15:49:42 +0530

    Removed Unused Imports

--- src/main/java/com/mongo/mongo/controller/ItemController.java
+++ src/main/java/com/mongo/mongo/controller/ItemController.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mongo.mongo.repository.ItemRepository;
 import com.mongo.mongo.model.GroceryItem;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
